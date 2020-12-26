55b392586c612c3958a2983ab934371bba6e3dfb
adiruj001 <adirujmukda001@gmail.com>
2020-12-26 20:24:52 +0700

    The first commit

---
+++ src/main/java/th/ac/ku/atm/ATMConfig.java
@@ -0,0 +1,29 @@
+package th.ac.ku.atm;
+
+import org.springframework.beans.factory.annotation.Configurable;
+import org.springframework.context.annotation.Bean;
+
+@Configurable
+public class ATMConfig {
+
+    @Bean
+    public DataSource dataSource() {
+        return new DataSourceFile("customers.txt");
+    }
+
+    @Bean
+    public Bank bank() {
+        return new Bank("Art Bank", dataSource());
+    }
+
+    @Bean
+    public ATM atm() {
+        return new ATM(bank());
+    }
+
+    @Bean
+    public AtmUI atmUI() {
+        return new AtmUI(atm());
+    }
+
+}
